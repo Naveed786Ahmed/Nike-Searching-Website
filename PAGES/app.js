da9a2659e7e791bd8752d01632512a07795db926
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2024-05-19 14:08:01 +0500

    Nike searching website

---
+++ PAGES/app.js
@@ -0,0 +1,237 @@
+let nike = {
+
+    jordan: {
+        Air_Jordan_1: {
+            name: "Air Jordan 1",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/01d62068-a591-4d4c-b47c-0cf16427bc82/air-jordan-1-low-og-shadow-shoes-v0FbJt.png",
+        },
+
+        Air_Jordan_11: {
+            name: "Air Jordan 11",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/0576137b-01b3-4e43-9a5a-b66645188d23/air-jordan-11-retro-low-black-varsity-royal-mens-shoes-MNm675.png",
+        },
+
+        Jordan_Air_Ship: {
+            name: "Jordan Air Ship",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f4bf6fe1-bc68-4af7-903c-b4354e4f89d5/jordan-air-ship-pe-sp-mens-shoes-5KVzjX.png",
+
+        },
+
+        Air_Jordan_3_Retro: {
+            name: "Air Jordan 3",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/95f1868d-b426-45b7-8e6b-0be358b0f479/air-jordan-3-retro-womens-shoes-1KkgvW.png",
+        },
+    },
+
+    Mens_Lifestyle_Shoes: {
+        Nike_Air_Max_90: {
+            name: "Nike Air Max 90",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8ea8c17b-45d1-483a-af6a-3edf863cd307/air-max-90-mens-shoes-Bd2qnn.png",
+
+        },
+        Nike_Go_FlyEase: {
+            name: "Nike Go FlyEase",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c76e2119-acb7-4944-9085-d4f5ae2bda4a/go-flyease-easy-on-off-shoes-LGmqKx.png",
+
+        },
+        Nike_Dunk_Low_Retro: {
+            name: "Nike Dunk Retro",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f8ea9265-77b0-4b69-8e33-275c99ef5915/dunk-low-retro-mens-shoes-1xlGBR.png",
+
+        },
+        Nike_Air_Max_90_GORE_TEX: {
+            name: "Nike Air GORE",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9f684ace-3163-4227-8f85-2d2a067dd4f5/air-max-90-gore-tex-mens-shoes-cZwz8t.png"
+
+        },
+    },
+
+    Mens_Walking_Shoes: {
+        Nike_InfinityRN_4: {
+            name: "Nike InfinityRN",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/822f0118-867c-4dff-9b94-c371ff02eb64/infinityrn-4-mens-road-running-shoes-hlqHFQ.png"
+
+        },
+
+        Nike_Dunk_Low_Retro: {
+            name: "Nike Dunk Retro",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f9d31118-d3fd-4336-bb85-a9fd8b1555fb/renew-ride-3-mens-road-running-shoes-7hTSLw.png"
+
+        },
+        Nike_Air_Monarch_IV: {
+            name: "Nike Monarch",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/a487f641-a6d6-4acc-9b49-79c70ddd6ee8/air-monarch-iv-amp-mens-workout-shoes-dChTJ7.png"
+
+        },
+        Nike_P_6000: {
+            name: "Nike P 6000",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/v88x3d5vx6oagsaacywh/p-6000-shoes-XkgpKW.png"
+
+        },
+    },
+
+    Mens_Football: {
+        Nike_Vapor_Edge_Pro_360_2: {
+            name: "Nike Vapor Edge Pro",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/729ef440-f13d-4988-b29a-2ad060eeb47a/vapor-edge-pro-360-2-mens-football-cleats-BZbhLL.png"
+
+        },
+
+        Nike_Alpha_Menace_4_Elite: {
+            name: "Nike Alpha Menace",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e6f2dade-8b8a-4546-a5d9-701adbd42c46/alpha-menace-4-pro-nrg-football-cleats-dvsFTl.png"
+
+        },
+
+        Jordan_1_Mid_TD: {
+            name: "Jordan 1 Mid Elite",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0f046044-3999-45de-ba86-5c34b7d7df49/vapor-edge-elite-360-2-mens-football-cleats-LdsghK.png"
+
+        },
+
+        Nike_Force_Savage: {
+            name: "Nike Force Savage",
+            category: "Men's Shoes",
+            discription: "Walk the path of greatness in the Air Jordan 1 Retro. These eternally popular kicks bring you sleek style with premium leather and a classic design that keeps the look as desirable as ever.",
+            color: "13 colors",
+            price: "160$",
+            image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/nv2jwqlloi72mt1pyq0e/force-savage-pro-2-mens-football-cleat-PHgwNl.png"
+
+        },
+    }
+
+}
+
+let value = document.getElementById("main")
+let catagory = document.getElementById("catagory")
+let design = document.getElementById("design")
+let detail = document.getElementById("watch-detail")
+
+catagory.innerHTML = `<option>Select Shoes Category</option>`
+design.innerHTML = `<option>Select Design</option>`
+
+
+for (let key in nike) {
+    catagory.innerHTML += `<option value = "${key}">${key}</option>`
+
+    for (let key1 in nike[key]) {
+        value.innerHTML += `<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-3">
+        <div class="card" style="width: 18rem;">
+            <img src="${nike[key][key1].image}" class="card-img-top" alt="..." height = "300px">
+            <div class="card-body">
+            <h2>${nike[key][key1].name}</h2>
+            <h6>${nike[key][key1].category}</h6>
+            <h6>${nike[key][key1].color}</h6>
+            <h6>${nike[key][key1].price}</h6>
+
+            </div>
+          </div>
+    </div>`
+    }
+}
+
+function setDesign() {
+    design.innerHTML = "";
+    design.innerHTML = `<option>Select Design</option>`
+
+
+    for (let key in nike[catagory.value]) {
+        design.innerHTML += `<option value = "${key}">${key}</option>`
+    }
+}
+
+function search() {
+    value.style.display = "none";
+    detail.style.display = "flex";
+    let shoes = nike[catagory.value][design.value];
+
+    detail.innerHTML = `<div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${shoes.image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h1 class="card-title">${shoes.name}</h1>
+          <p class="card-text">"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita vel repellat facere repellendus id, nobis suscipit adipisci nulla ad voluptates, ratione debitis cum assumenda dolorem saepe. Nisi culpa quis nemo.
+          Ut atque voluptates explicabo. Ipsum, eaque. Nobis ratione esse laudantium laborum consectetur repellendus totam voluptas molestias assumenda ullam dicta, voluptatem cupiditate nisi pariatur fuga. Iure dicta impedit sunt odit consectetur!
+          Tempora, sit inventore labore beatae omnis dolore. Mollitia voluptas laboriosam odit libero commodi temporibus impedit dolor adipisci porro dignissimos! Quod porro corporis eius pariatur non rem libero soluta id saepe!"</p>
+          <h6 class="card-text">Category: ${shoes.category}</h6>
+          <h6 class="card-text">Color: ${shoes.color}</h6>
+          <h6 class="card-text">Price: ${shoes.price}</h6>
+          <br><br><br>
+          <button type="button" onclick = "feedbacks() " class="btn btn-primary">Order Now</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+}
+
+function refresh() {
+    detail.style.display = "none";
+    value.style.display = "flex";
+}
+
+function feedbacks() {
+    alert("Thank You For Visited My Website")
+}
